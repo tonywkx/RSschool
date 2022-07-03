8705629cc07972394cb56766357d71ef59775d0c
tonywkx <tonywk@ya.ru>
2022-07-03 22:19:20 +0400

    fix: fix some dependences

--- migration-to-ts/webpack.config.js
+++ migration-to-ts/webpack.config.js
@@ -4,6 +4,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.ts'),
@@ -27,7 +28,7 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js', '.ts'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
@@ -51,6 +52,7 @@ const baseConfig = {
               },
             ],
           }),
+          new EslingPlugin({ extensions: 'ts' })
     ],
 };
 
