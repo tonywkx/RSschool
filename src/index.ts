b5ee2cb0f3b0605f9f01034137858cc464b52b2b
tonywkx <tonywk@ya.ru>
2022-08-02 00:08:55 +0400

    feat: add webpack, linter etc.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import './style.css';
+
+const f = document.createElement('div');
+f!.innerHTML = "GOOOD";
+f.classList.add('text');
+document.body.appendChild(f);
+const k = document.createElement('div');
+k.classList.add('cube');
+document.body.appendChild(k);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+/* const baseConfig */ module.exports = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    devtool: 'eval-source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                include: [path.resolve(__dirname, 'src')],
+                use: 'ts-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+              {
+                from: path.resolve(__dirname, 'src/assets/favicon.ico'),
+                to: path.resolve(__dirname, 'dist'),
+              },
+              
+            ],
+          }),
+          new EslingPlugin({ extensions: 'ts' })
+    ],
+};
+
+/* module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+}; */
